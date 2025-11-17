059eacba7e06958d10e9e23db3ed0ead4914494c
sangwonchoi <rngus3698@naver.com>
2025-11-17 12:34:17 +0900

    configModel import app.module.ts -> core.module.ts 변경

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoreModule } from '../core/core.module';
-import { ConfigModule } from '@nestjs/config';
-import configuration from '../core/config/configuration';
 import { FeaturesModule } from '../feature/feature.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true, // 다른 모듈에서 별도의 설정없이 환경변수의 사용이 가능합니다.
-      load: [configuration],
-      envFilePath: [
-        `${process.cwd()}/dist/core/config/env/.env.${process.env.NODE_ENV}`,
-      ],
-    }),
     CoreModule,
     FeaturesModule,
   ],
--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 다른 모듈에서 별도의 설정없이 환경변수의 사용이 가능합니다.
+      load: [configuration],
+      envFilePath: [
+        `${process.cwd()}/dist/core/config/env/.env.${process.env.NODE_ENV}`,
+      ],
+    }),
     DatabaseModule,
   ]
 })
